c2f6ab20ff822cda19ac350a9707f8f245bb6221
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-09 12:53:57 -0700

    Classes

--- src/15_classes.py
+++ src/15_classes.py
@@ -2,28 +2,62 @@
 # constructor
 
 # YOUR CODE HERE
-        
+# self === coord here
+class LatLon():
+    def __init__(coord, lat, lon):
+        coord.lat = lat
+        coord.lon = lon
+
+c1 = LatLon(55, 55)
+print("C1 lat:", c1.lat)
+print("C1 lon:", c1.lon)
+
+c2 = LatLon(66, 66)
+print("C2 lat:", c2.lat)
+print("C2 lon:", c2.lon)
+
 # Make a class Waypoint that can be passed parameters `name`, `lat`, and `lon` to the
 # constructor. It should inherit from LatLon. Look up the `super` method.
 
 # YOUR CODE HERE
+class Waypoint(LatLon):
+    def __init__(self, name, lat, lon):
+        super().__init__(lat, lon)
+        self.name = name
+    
+    # increasing readability
+    def __str__(self):
+        return 'Waypoint( name = '+self.name+', lat = '+str(self.lat)+', lon = '+str(self.lon)+' )'
 
 # Make a class Geocache that can be passed parameters `name`, `difficulty`,
 # `size`, `lat`, and `lon` to the constructor. What should it inherit from?
 
 # YOUR CODE HERE
+class Geocache(Waypoint):
+    def __init__(self, name, difficulty, size, lat, lon):
+        super().__init__(name, lat, lon)
+        self.difficulty = difficulty
+        self.size = size
+
+    # increasing readability
+    def __str__(self):
+        return 'Geocache( name = '+self.name+', difficulty = '+str(self.difficulty)+', size = '+str(self.size)+', lat = '+str(self.lat)+', lon = '+str(self.lon)+' )'
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
 # YOUR CODE HERE
+w1 = Waypoint("Catacombs", 41.70505, -121.51521)
+print(f'"{w1.name}", {w1.lat}, {w1.lon}')
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint)
+print(w1)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
 # YOUR CODE HERE
+g1 = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
+print(f'"{g1.name}", diff {g1.difficulty}, size {g1.size}, {g1.lat}, {g1.lon}')
 
 # Print it--also make this print more nicely
-print(geocache)
+print(g1)
