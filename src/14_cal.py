7a42cc5c007cd0b66e86b9282ab1867273574258
AmarachiOE <ao.ejiawoko@gmail.com>
2019-07-09 13:42:41 -0700

    Calendar. Finally.

--- src/14_cal.py
+++ src/14_cal.py
@@ -22,3 +22,49 @@ and does the following:
 import sys
 import calendar
 from datetime import datetime
+
+num_args = len(sys.argv)
+if num_args == 1:
+    # print calendar for current month in current year (July 2019)
+    current_month = datetime.now().month
+    current_year = datetime.now().year    
+    print(current_month, current_year)
+    calendar.TextCalendar().prmonth(current_year, current_month)
+    
+elif num_args == 2:
+    # render calendar for that month of current year
+    current_year = datetime.now().year    
+    month = int(sys.argv[1])
+    print(month, current_year)
+    calendar.TextCalendar().prmonth(current_year, month)
+
+elif num_args == 3:
+    # render calendar for that month and year
+    month = int(sys.argv[1])
+    year = int(sys.argv[2])
+    print(month, year)
+    calendar.TextCalendar().prmonth(year, month)
+else:
+    print("Expected format: 14_cal.py month year ")
+
+
+# EXTRA NOTES
+"""
+from datetime import datetime
+
+currentSecond= datetime.now().second
+currentMinute = datetime.now().minute
+currentHour = datetime.now().hour
+
+currentDay = datetime.now().day
+currentMonth = datetime.now().month
+currentYear = datetime.now().year
+
+Entire Year:
+calendar.TextCalendar().pryear(2019)
+
+Specific month from specific year:
+calendar.TextCalendar().prmonth(year, month)
+
+
+"""
